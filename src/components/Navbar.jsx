3c4e3c984d752de023d7ccca95d5eabe7dddd654
Ushasri17 <usri71168@gmail.com>
2026-03-02 17:34:05 +0530

    Updated navbar layout and search UI styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,78 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import MovieCard from "./MovieCard";
 import "./Navbar.css";
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="logo">CINIVERSE</div>
+  const [openSearch, setOpenSearch] = useState(false);
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/tvshows">TV Shows</Link>
-      </div>
-    </nav>
+  const API_KEY = "7b0654a";
+
+  useEffect(() => {
+    if (!query) {
+      setMovies([]);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.Response === "True") {
+            setMovies(data.Search);
+          } else {
+            setMovies([]);
+          }
+        });
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [query]);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-left">
+          <div className="logo">CINIVERSE</div>
+
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/movies">Movies</Link>
+            <Link to="/tvshows">TV Shows</Link>
+          </div>
+        </div>
+
+        <div className="nav-right">
+          <FaSearch
+            className="search-icon"
+            onClick={() => setOpenSearch(!openSearch)}
+          />
+        </div>
+      </nav>
+
+      {openSearch && (
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search movies or series..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            autoFocus
+          />
+
+          <div className="search-results">
+            {movies.length > 0
+              ? movies.map((movie) => (
+                  <MovieCard key={movie.imdbID} movie={movie} />
+                ))
+              : query && <p className="not-found">No results found</p>}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
 export default Navbar;
